f959dc70e7e2e5ad030a3827da8d9ff53dc23c7c
Royce Schultz <royceschultz1@gmail.com>
2023-06-25 23:05:25 -0600

    Minor changes to header.

--- frontend-ts/src/App.tsx
+++ frontend-ts/src/App.tsx
@@ -14,36 +14,43 @@ function App() {
 
   const [currentPage, setCurrentPage] = useState(pages[0].name);
 
+  const Nametag = () => (
+    <div className='bg-white p-2 rounded-md shadow-md flex flex-row'>
+      <div className='text-lg font-bold'>Royce Schultz</div>
+      <div className='text-xs flex flex-row'>
+        <a href='https://www.linkedin.com/in/royceschultz/' target='_blank'>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="rgb(10, 102, 194)" width="24" height="24" focusable="false">
+            <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
+          </svg>
+        </a>
+        <a href='https://github.com/royceschultz'>
+          <svg aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="24" height="24" data-view-component="true">
+            <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+          </svg>
+        </a>
+      </div>
+    </div>
+  )
+
 
   return (
     <div className='h-screen w-screen'>
       <GlobalContextProvider>
         <div className='h-full w-full flex flex-col'>
-          <div className={styles.PageHeader}>
-            <div className={styles.SocialLinksContainer}>
-              <h1 className='text-3xl font-bold underline'>RoyceSchultz.com</h1>
-              <a href='https://www.linkedin.com/in/royceschultz/' target='_blank'>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="rgb(10, 102, 194)" width="24" height="24" focusable="false">
-                  <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
-                </svg>
-              </a>
-
-              <a href='https://github.com/royceschultz'>
-                <svg aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="24" height="24" data-view-component="true">
-                  <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
-                </svg>
-              </a>
-
+          <div className='flex flex-row content-center'>
+            <Nametag />
+            <div className={styles.PageButtonContainer}>
+              {pages.map((page) => (
+                <div
+                  className='group cursor-pointer rounded-md p-3 m-1 ring-slate-200 shadow-sm  hover:shadow-md bg-slate-700 ring-0 highlight-white/10 hover:bg-blue-500'
+                  key={page.name} onClick={() => { setCurrentPage(page.name) }}>
+                  {page.name}
+                </div>
+              ))}
             </div>
-          </div>
-          <div className={styles.PageButtonContainer}>
-            {pages.map((page) => (
-              <div
-                className='group cursor-pointer rounded-md p-3 m-1 ring-slate-200 shadow-sm  hover:shadow-md bg-slate-700 ring-0 highlight-white/10 hover:bg-blue-500'
-                key={page.name} onClick={() => { setCurrentPage(page.name) }}>
-                {page.name}
-              </div>
-            ))}
+            <div className='text-lg font-bold text-yellow-300 p-3'>
+              Development Preview
+            </div>
           </div>
           <div className={styles.PageComponentContainer}>
             {
